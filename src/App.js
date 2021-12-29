878aa3aefe2914309bbd3cab34682454692c46d6
Mounika <bojjamounika8@gmail.com>
2021-12-29 12:53:54 +0530

    usestate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Counter({initialCount}) {
+  const [count, setCount] = useState(initialCount);
+  return (
+    <>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+    </>
+  );
+}
+
+export default Counter;
